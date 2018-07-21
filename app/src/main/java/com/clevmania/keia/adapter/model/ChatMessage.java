a657d9aed358670b876096849539722077aed07d
clevmania <lawrence@grandilo.com>
2018-07-21 16:17:11 +0100

    created chat model class

---
+++ app/src/main/java/com/clevmania/keia/adapter/model/ChatMessage.java
@@ -0,0 +1,32 @@
+package com.clevmania.keia.adapter.model;
+
+import java.io.Serializable;
+
+public class ChatMessage implements Serializable {
+    private String id;
+    private String msg;
+
+    public ChatMessage() {
+    }
+
+    public ChatMessage(String id, String msg) {
+        this.id = id;
+        this.msg = msg;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
