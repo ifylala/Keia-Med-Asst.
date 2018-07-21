ed02f50cd13dade3d835f125d223283d2082613c
clevmania <lawrence@grandilo.com>
2018-07-21 16:29:22 +0100

    created symptoms adapter class

--- app/src/main/java/com/clevmania/keia/adapter/SymptomsAdapter.java
+++ app/src/main/java/com/clevmania/keia/adapter/SymptomsAdapter.java
@@ -2,32 +2,60 @@ package com.clevmania.keia.adapter;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.clevmania.keia.R;
+import com.clevmania.keia.adapter.model.ChatMessage;
 
 import java.util.ArrayList;
 
 public class SymptomsAdapter extends RecyclerView.Adapter<SymptomsAdapter.ViewHolder> {
-    private int SELF = 100;
+    private static final int SELF_MESSAGE = 100;
+    private ArrayList<ChatMessage> chatList;
 
+    public SymptomsAdapter(ArrayList<ChatMessage> chatList) {
+        this.chatList = chatList;
+    }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View itemView;
+        if (viewType == SELF_MESSAGE) {
+
+            itemView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_message, parent, false);
+        } else {
+
+            itemView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_bot_message, parent, false);
+        }
+
+        return new ViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        ChatMessage message = chatList.get(position);
+        holder.messageView.setText(message.getMsg());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return chatList.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        ChatMessage message = chatList.get(position);
+        if (message.getId()!=null && message.getId().equals("1")) {
+            return SELF_MESSAGE;
+        }
+
+        return position;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
