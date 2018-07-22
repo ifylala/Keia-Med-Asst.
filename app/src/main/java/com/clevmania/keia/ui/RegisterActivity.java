428de28e6bd2aca015d49f88c0834d9012a75bbb
clevmania <lawrence@grandilo.com>
2018-07-22 08:37:48 +0100

    completed register ui

--- app/src/main/java/com/clevmania/keia/ui/RegisterActivity.java
+++ app/src/main/java/com/clevmania/keia/ui/RegisterActivity.java
@@ -1,15 +1,20 @@
 package com.clevmania.keia.ui;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.clevmania.keia.R;
 
 public class RegisterActivity extends AppCompatActivity {
+    private TextView headerText, or;
+    private Button google, facebook;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +23,16 @@ public class RegisterActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        headerText = findViewById(R.id.tv_header);
+        or = findViewById(R.id.tv_or);
+        facebook = findViewById(R.id.btn_facebook);
+        google = findViewById(R.id.btn_google);
+
+        String customFont = "Nunito-SemiBold.ttf";
+        Typeface header = Typeface.createFromAsset(getAssets(),customFont);
+        headerText.setTypeface(header);
+        or.setTypeface(header);
+
     }
 
 }
