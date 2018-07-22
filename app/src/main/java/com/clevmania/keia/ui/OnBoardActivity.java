7996820fb0b8f7f29b59dafcd5f8200ea8306348
clevmania <lawrence@grandilo.com>
2018-07-22 01:18:16 +0100

    designed and implemented boarding screen

---
+++ app/src/main/java/com/clevmania/keia/ui/OnBoardActivity.java
@@ -0,0 +1,134 @@
+package com.clevmania.keia.ui;
+
+import android.content.Intent;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.clevmania.keia.MainActivity;
+import com.clevmania.keia.R;
+import com.clevmania.keia.adapter.SliderAdapter;
+
+public class OnBoardActivity extends AppCompatActivity {
+    private ViewPager onBoardPager;
+    private LinearLayout navLayout;
+    private SliderAdapter sliderAdapter;
+    private TextView navDots[];
+    private Button next,prev;
+    private int currentPage;
+    private ViewPager.OnPageChangeListener slidePageListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_board);
+
+        onBoardPager = findViewById(R.id.vp_slider);
+        navLayout = findViewById(R.id.ll_nav_layout);
+        sliderAdapter = new SliderAdapter(this);
+
+        next = findViewById(R.id.btn_next);
+        prev = findViewById(R.id.btn_skip);
+
+        onBoardPager.setAdapter(sliderAdapter);
+        displayDots(0);
+
+        slidePageListener = new ViewPager.OnPageChangeListener(){
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                displayDots(position);
+                currentPage = position;
+
+                if(currentPage == 0){
+                    next.setEnabled(true);
+                    prev.setEnabled(false);
+                    prev.setVisibility(View.INVISIBLE);
+
+                    next.setText("Next");
+                    prev.setText("");
+
+                }else if(currentPage == navDots.length - 1 ){
+                    next.setEnabled(true);
+                    prev.setEnabled(true);
+                    prev.setVisibility(View.VISIBLE);
+
+                    next.setText("Continue");
+                    prev.setText("Previous");
+                }else {
+                    next.setEnabled(true);
+                    prev.setEnabled(true);
+                    prev.setVisibility(View.VISIBLE);
+
+                    next.setText("Next");
+                    prev.setText("Previous");
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        };
+
+        onBoardPager.addOnPageChangeListener(slidePageListener);
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int currentItem = getItem(+1);
+                if(currentItem < navDots.length){
+                    onBoardPager.setCurrentItem(currentItem);
+                }else {
+                    // Todo intent should fire signin activity
+                    Intent intent = new Intent(OnBoardActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        });
+
+        prev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int currentItem = getItem(-1);
+                if(currentItem < navDots.length){
+                    onBoardPager.setCurrentItem(currentItem);
+                }
+            }
+        });
+    }
+
+    private void displayDots(int position){
+        navDots = new TextView[3];
+        navLayout.removeAllViews();
+
+        for(int i = 0; i<navDots.length; i++){
+            navDots[i] = new TextView(this);
+            navDots[i].setText(Html.fromHtml("&#8226"));
+            navDots[i].setTextSize(35);
+            navDots[i].setTextColor(getResources().getColor(R.color.colorPrimary));
+
+            navLayout.addView(navDots[i]);
+        }
+
+        if(navDots.length > 0){
+            navDots[position].setTextColor(getResources().getColor(R.color.colorAccent));
+        }
+
+    }
+
+    private int getItem(int i){
+        return onBoardPager.getCurrentItem() + i;
+    }
+}
